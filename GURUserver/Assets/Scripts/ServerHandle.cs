3fa78fbd90fb49ba43d9b315f5d3f93f5ae0dc69
BobersArmy <62320586+BobersArmy@users.noreply.github.com>
2020-08-26 19:33:10 -0400

    Unfinished
    Can send messages from client to server and back. Not much yet. No message box.

---
+++ GURUserver/Assets/Scripts/ServerHandle.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ServerHandle
+{
+    public static void WelcomeReceived(int _fromClient, Packet _packet)
+    {
+        int _clientIdCheck = _packet.ReadInt();
+        string _username = _packet.ReadString();
+
+        Debug.Log($"{Server.clients[_fromClient].tcp.socket.Client.RemoteEndPoint} connected successfully and is now player {_fromClient}.");
+        if (_fromClient != _clientIdCheck)
+        {
+            Debug.Log($"Player \"{_username}\" (ID: {_fromClient}) has assumed the wrong client ID ({_clientIdCheck})!");
+        }
+    }
+
+    public static void RecieveMessage(int _fromClient, Packet _packet)
+    {
+        Debug.Log(_packet.ReadString());
+    }
+}
